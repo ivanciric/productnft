985a1991b6b9057ce50e2d99911f8d89e43fb637
Ivan Ciric <info@yoshi.tech>
2024-05-28 14:54:31 +0200

    propper translatable string

--- productnft.php
+++ productnft.php
@@ -24,7 +24,7 @@ add_action('admin_init', function () {
 });
 
 add_filter('plugin_action_links_' . plugin_basename(__FILE__), function ($links) {
-    $settings_link = '<a href="admin.php?page=productnft-settings">' . __('Settings') . '</a>';
+    $settings_link = '<a href="admin.php?page=productnft-settings">' . esc_html__('Settings', 'productnft') . '</a>';
     array_unshift($links, $settings_link);
     return $links;
 });
